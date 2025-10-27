fa28466982810d482e53856a7e006ed25daab2ca
Abdullahi Kabri <kabriacid01@gmail.com>
2025-10-27 22:44:12 +0000

    Always hide the navbar in the /details page.

--- src/app/redeem/details/page.tsx
+++ src/app/redeem/details/page.tsx
@@ -1,24 +1,9 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { DetailsForm } from '@/components/details-form';
-import { Logo } from '@/components/logo';
-import Link from 'next/link';
-import { Button } from '@/components/ui/button';
-import { ArrowLeft } from 'lucide-react';
 
 export default function RedeemDetailsPage() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-background p-4">
-       <header className="absolute top-0 left-0 w-full p-4 md:p-6">
-        <div className='flex items-center justify-between'>
-            <Logo />
-             <Button asChild variant="ghost">
-                <Link href="/">
-                    <ArrowLeft className="mr-2 h-4 w-4" />
-                    Back to Home
-                </Link>
-            </Button>
-        </div>
-      </header>
       <main className="w-full max-w-2xl">
         <Card className="shadow-2xl">
           <CardHeader className="text-center">
