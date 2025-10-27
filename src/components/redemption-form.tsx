54e5652f3d4d469a5256babd5826828611c52201
Abdullahi Kabri <kabriacid01@gmail.com>
2025-10-27 22:59:23 +0000

    Try fixing this error: `Runtime Error: Error: (0 , {imported module [pro

--- src/components/redemption-form.tsx
+++ src/components/redemption-form.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { useActionState, useFormStatus } from 'react-dom';
+import { useActionState } from 'react';
+import { useFormStatus } from 'react-dom';
 import { useEffect } from 'react';
 import { AlertCircle, ArrowRight, CheckCircle, LoaderCircle } from 'lucide-react';
 
