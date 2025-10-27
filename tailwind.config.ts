360611a0616182812fdd243a4bae874b2f75b63e
Abdullahi Kabri <kabriacid01@gmail.com>
2025-10-27 22:51:50 +0000

    Remove Montserrat and use Plus Jakarta Sans.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Montserrat', 'sans-serif'],
+        sans: ['"Plus Jakarta Sans"', 'sans-serif'],
       },
       colors: {
         background: 'hsl(var(--background))',
